5dc262ea2d5263935093153ea96fe7e6df4c1ec3
aryan malwa <aryanmalwa07@gmail.com>
2025-10-28 21:57:22 +0530

    console

--- server/api/index.js
+++ server/api/index.js
@@ -47,10 +47,18 @@ app.use(authMiddleware);
 let isConnected = false;
 async function connectDB() {
   if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  // await mongoose.connect(process.env.MONGO_URI, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+  await mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("✅ MongoDB Connected Successfully"))
+    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
   isConnected = true;
   console.log("✅ MongoDB connected");
 }
